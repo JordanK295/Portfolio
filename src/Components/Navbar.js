8a30d0eff644dcd4c20e03f5a302a022ce02907e
Jordan Kay <jkay10100@gmail.com>
2020-11-12 13:54:40 +0000

    Navbar color on hover + blank target footer links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,29 +18,53 @@ const Navbar = () => {
     aboutPage.scrollIntoView();
   }
 
-  // Creating the sticky navbar effect and filling the gap with margin when it swaps position to fixed
-
-  const handleScroll = (sticky) => {
+  const handleScroll = (sticky, projectsOffset, aboutOffset) => {
+      // Creating the sticky navbar effect and filling the gap with margin when it swaps position to fixed
     var navBar = document.getElementById("navbar");
     var projectTitle = document.getElementById("project-title");
     const shouldFix = window.pageYOffset > sticky;
     projectTitle.style.marginTop = shouldFix ? "102px" : "50px";
-    navBar.className = shouldFix ? "navbar Nav__black" : "navbar";
+    navBar.className = shouldFix ? "navbar Nav__fixed" : "navbar";
+
+    // Navbar coloring based on scroll position
+    var navbarAbout = document.getElementById("navbar-about");
+    var navbarProjects = document.getElementById("navbar-projects");
+    var navbarHome = document.getElementById("navbar-home");
+
+    if (window.pageYOffset > aboutOffset) {
+      navbarAbout.className = "navbar-about highlighted"
+      navbarProjects.className = "navbar-projects un-highlighted"
+      navbarHome.className = "navbar-home un-highlighted"
+    }
+    else if (window.pageYOffset > projectsOffset) {
+      navbarAbout.className = "navbar-about un-highlighted"
+      navbarProjects.className = "navbar-projects highlighted"
+      navbarHome.className = "navbar-home un-highlighted"
+    }
+    else {
+      navbarAbout.className = "navbar-about un-highlighted"
+      navbarProjects.className = "navbar-projects un-highlighted"
+      navbarHome.className = "navbar-home highlighted"
+    }
   }
 
   useEffect(() => {
     var navBar = document.getElementById("navbar");
+    var projects = document.getElementById("projects-container-wrapper");
+    var about = document.getElementById("about");
     var sticky = navBar.offsetTop;
-    window.addEventListener("scroll", () => handleScroll(sticky));
+    var projectsOffset = projects.offsetTop -300
+    var aboutOffset = about.offsetTop - 300
+    window.addEventListener("scroll", () => handleScroll(sticky, projectsOffset, aboutOffset));
   }, [])
 
   return (
     <div>
         <div className="navbar" id="navbar">
           <div className="links">
-            <div style={{ color: '#E94C72' }} onClick={scrollToHome}>HOME</div>
-            <div onClick={scrollToProjects}>PROJECTS</div>
-            <div onClick={scrollToAbout}>ABOUT</div>
+            <div onClick={scrollToHome} id="navbar-home">HOME</div>
+            <div onClick={scrollToProjects} id="navbar-projects">PROJECTS</div>
+            <div onClick={scrollToAbout} id="navbar-about">ABOUT</div>
           </div>
         </div>
     </div>
